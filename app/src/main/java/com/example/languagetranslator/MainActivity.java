65afa40c747973a6431a5126f829f53e67f75284
Sai Nishwanth Raj <sainishwanthraj@gmail.com>
2022-05-23 17:02:55 +0530

    Added a Spinner

--- app/src/main/java/com/example/languagetranslator/MainActivity.java
+++ app/src/main/java/com/example/languagetranslator/MainActivity.java
@@ -5,33 +5,106 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 import com.google.android.material.button.MaterialButton;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText from;
-    private EditText to;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner fromspinner;
+    private Spinner tospinner;
+    private String fromString;
+    private String toString;
     private Translate_main obj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        from = (EditText) findViewById(R.id.FromLang);
-        to = (EditText) findViewById(R.id.ToLang);
+        fromspinner = findViewById(R.id.fromspinner);
+        tospinner =  findViewById(R.id.tospinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.language, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        try{
+            fromspinner.setAdapter(adapter);
+            tospinner.setAdapter(adapter);
+        }catch(NullPointerException e){}
+        fromspinner.setOnItemSelectedListener(this);
+        tospinner.setOnItemSelectedListener(this);
         MaterialButton submitButton = (MaterialButton) findViewById(R.id.submit);
-
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (from.getText().toString().equals("") || to.getText().toString().equals("")) {
-                    Toast.makeText(MainActivity.this, "Fields Cannot be Empty", Toast.LENGTH_SHORT).show();
+                if (fromString.equals("Select") || toString.equals("Select")) {
+                    Toast.makeText(MainActivity.this, "Required Fields Cannot be Empty", Toast.LENGTH_SHORT).show();
                 } else {
-                    obj = new Translate_main(from.getText().toString(), to.getText().toString());
+                    switch (fromString){
+                        case "English":
+                            fromString = "en";
+                            break;
+                        case "Hindi":
+                            fromString = "hi";
+                            break;
+                        case "French":
+                            fromString = "fr";
+                            break;
+                        case "Japanese":
+                            fromString = "ja";
+                            break;
+                        case "German":
+                            fromString = "de";
+                            break;
+                        case "Spanish":
+                            fromString = "es";
+                            break;
+                        case "Kannada":
+                            fromString = "kn";
+                            break;
+                    }
+                    switch (toString) {
+                        case "English":
+                            toString = "en";
+                            break;
+                        case "Hindi":
+                            toString = "hi";
+                            break;
+                        case "French":
+                            toString = "fr";
+                            break;
+                        case "Japanese":
+                            toString = "ja";
+                            break;
+                        case "German":
+                            toString = "de";
+                            break;
+                        case "Spanish":
+                            toString = "es";
+                            break;
+                        case "Kannada":
+                            toString = "kn";
+                            break;
+                    }
+                    obj = new Translate_main(fromString, toString);
                     Intent intent = new Intent(getApplicationContext(), Translate_main.class);
                     startActivity(intent);
                 }
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+         switch (parent.getId()){
+             case R.id.fromspinner:
+                 fromString = parent.getItemAtPosition(position).toString();
+                 break;
+             case R.id.tospinner:
+                 toString = parent.getItemAtPosition(position).toString();
+                 break;
+         }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
     }
 }
